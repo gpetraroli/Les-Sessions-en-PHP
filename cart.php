3b1bcd030e23be7619426dbacfabae65d79a7255
Giuseppe Petraroli <g.petraroli@gmail.com>
2022-04-19 10:48:54 +0200

    refactoring

--- cart.php
+++ cart.php
@@ -25,24 +25,27 @@ if(isset($_GET['remove_from_cart']))
 <section class="cookies container-fluid">
     <div class="row">
         <ul class="cart-list">
-        <?php foreach ($_SESSION['cart'] as $id => $product): ?>
-        <?php if($product['qta'] !== 0): ?>
-            <li class="cart-list__item list-unstyled">
-                <a href="?remove_from_cart=<?= $id; ?>" class="btn btn-primary">
-                    <span class="glyphicon glyphicon-minus" aria-hidden="true"></span>
-                </a>
-                <p class="cart-list__text">
-                <?= $product['qta'] ?>
-                x
-                <?= $product['name'] ?>
-                </p>
-            </li>
+            <?php foreach ($_SESSION['cart'] as $id => $product): ?>
+                <?php if($product['qta'] !== 0): ?>
+                    <li class="cart-list__item list-unstyled">
+                        <a href="?remove_from_cart=<?= $id; ?>" class="btn btn-primary">
+                            <span class="glyphicon glyphicon-minus" aria-hidden="true"></span>
+                        </a>
+                        <p class="cart-list__text">
+                            <?= $product['qta'] ?>
+                            x
+                            <?= $product['name'] ?>
+                        </p>
+                    </li>
+                <?php endif; ?>
+            <?php endforeach; ?>
+            <?php if(array_sum(array_column($_SESSION['cart'], 'qta')) === 0): ?>
+                <li class="cart-list__item list-unstyled">
+                    <p>Your cart is empty! <a href="index.php">Continue shopping</a></p>
+                </li>
             <?php endif; ?>
-        <?php endforeach; ?>
         </ul>
-        <?php if(array_sum(array_column($_SESSION['cart'], 'qta')) === 0): ?>
-            <p>Your cart is empty! <a href="index.php">Continue shopping</a></p>
-        <?php endif; ?>
+
     </div>
 </section>
 <?php require 'inc/foot.php'; ?>
